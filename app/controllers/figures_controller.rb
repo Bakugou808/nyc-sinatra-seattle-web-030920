a2b265d46b72a9d4dd84ff1ee73feb33b11d1b78
Bakugou808 <janu.m.sung@gmail.com>
2020-04-02 18:31:47 -0700

    Sinatra NYC Pair Lab

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,80 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  #Create
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do 
+    create_fig_params = {name: params[:figure][:name], title_ids: params[:figure][:title_ids], landmark_ids: params[:figure][:landmark_ids]}
+    @figure = Figure.create(create_fig_params)
+    if !params[:figure][:title][:name].empty?
+      new_title = Title.create(params[:figure][:title])
+      @figure.titles << new_title 
+    end 
+    if !params[:figure][:landmark][:name].empty? && !params[:figure][:landmark][:year_completed].empty?
+      new_landmark = Landmark.create(params[:figure][:landmark])
+      @figure.landmarks << new_landmark 
+    end 
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  #Read
+  #Read(all) --> /index will display all instances of figure
+
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  #read instance
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end 
+
+
+  #Update
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    @titles = Title.all 
+    @landmarks = Landmark.all 
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do 
+    update_fig_params = {title_ids: params[:figure][:title_ids], landmark_ids: params[:figure][:landmark_ids]}
+    @figure = Figure.find(params[:id])
+    @figure.update(update_fig_params)
+    if !params[:figure][:title][:name].empty?
+      new_title = Title.create(params[:figure][:title])
+      @figure.titles << new_title 
+    end 
+    if !params[:figure][:landmark][:name].empty? && !params[:figure][:landmark][:year_completed].empty?
+      new_landmark = Landmark.create(params[:figure][:landmark])
+      @figure.landmarks << new_landmark 
+    end 
+
+    redirect to "/figures/#{@figure.id}"
+    
+  end 
+
+
+  #Delete
+
+  delete '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    @figure.delete 
+
+    redirect to '/figures'
+
+  end
+
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+
+  #read all
+
+  get '/landmarks' do 
+    @landmarks = Landmark.all 
+    erb :'landmarks/index'
+  end
+
+  #read instance
+
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  #create
+
+  # get '/landmarks/new' do 
+  #   erb :'landmarks/new'
+  # end
+
+  # post '/landmarks' do 
+  #   binding.pry
+  #   @landmark = Landmark.create(params[:landmark]) ---> Landmark.create(name: "eifel tower", figure_id: 3, year_completed: 1698)
+  # end
+
+
 end
